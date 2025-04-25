4682c2f591227f060450518f20031255b0c7054f
Sean McCarty <28798809+seanmccarty@users.noreply.github.com>
2025-04-24 19:26:21 -0500

    Fix 5E breaks due to recent ruleset changes

---
+++ campaign/CSDT_5E.lua
@@ -0,0 +1,41 @@
+function onInitAction(draginfo)
+	if OptionsManager.isOption("TBOX", "on") and OptionsManager.isOption("CSDT_option_label_initiative", "on") then
+		local rActor = ActorManager.resolveActor(getDatabaseNode());
+		local rRoll = ActionInit.getRoll(rActor);
+		ActionsManager.applyModifiers(rActor, nil, rRoll);
+		DiceTowerManager.sendRoll(rRoll, rActor);
+		return true;
+	else
+		return super.onInitAction(draginfo);
+	end
+end
+
+function onCheckAction(draginfo, sCheck)
+	if (sCheck or "") == "" then
+		return false;
+	end
+	if OptionsManager.isOption("TBOX", "on") and OptionsManager.isOption("CSDT_option_label_ability", "on") then
+		local rActor = ActorManager.resolveActor(getDatabaseNode());
+		local rRoll = ActionCheck.getRoll(rActor, sCheck);
+		ActionsManager.applyModifiers(rActor, nil, rRoll);
+		DiceTowerManager.sendRoll(rRoll, rActor);
+		return true;
+	else
+		return super.onCheckAction(draginfo, sCheck);
+	end
+end
+
+function onSaveAction(draginfo, sSave)
+	if (sSave or "") == "" then
+		return false;
+	end
+	if OptionsManager.isOption("TBOX", "on") and OptionsManager.isOption("CSDT_option_label_save", "on") then
+		local rActor = ActorManager.resolveActor(getDatabaseNode());
+		local rRoll = ActionSave.getRoll(rActor, sSave);
+		ActionsManager.applyModifiers(rActor, nil, rRoll);
+		DiceTowerManager.sendRoll(rRoll, rActor);
+		return true;
+	else
+		super.onSaveAction(draginfo, sSave)
+	end
+end
